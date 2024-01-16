76647727b33aae93fad107f998fcea2dbdb047a4
Oracso <85285983+Oracso@users.noreply.github.com>
2024-01-16 13:38:24 +0000

    Initial Commit

---
+++ CVTemplate/CVTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  CVTemplateApp.swift
+//  CVTemplate
+//
+//  Created by Oscar Hardy on 16/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct CVTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
